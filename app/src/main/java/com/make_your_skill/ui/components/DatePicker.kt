029c3ef88e90a1133bc0ded9802e59bd2ee35075
AlexLangman <alulang@hotmail.com>
2024-10-30 18:26:39 -0300

    adding calendar icon

--- app/src/main/java/com/make_your_skill/ui/components/DatePicker.kt
+++ app/src/main/java/com/make_your_skill/ui/components/DatePicker.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,10 +24,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
+import com.make_your_skill.R
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -49,7 +50,7 @@ fun DatePickerDocked(datePickerState: DatePickerState, selectedDate: String) {
             readOnly = true,
             trailingIcon = {
                 IconButton(onClick = { showDatePicker = !showDatePicker }) {
-                    Text("C") //Aca abria que poner un Icon pero no me lo importa bien
+                    Icon(painter = painterResource(id = R.drawable.calendar), contentDescription = "calendar")
                 }
             },
             modifier = Modifier
