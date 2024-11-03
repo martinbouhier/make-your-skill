cd30e527ca5fb5169d459b35b52cd898152b4745
Martin Rivas <dibuyo@gmail.com>
2024-11-03 12:42:09 -0300

    Add Default ViewModel with Factory & Inyect Service Repository

--- app/src/main/java/com/make_your_skill/ui/screens/singIn/SingInRoutes.kt
+++ app/src/main/java/com/make_your_skill/ui/screens/singIn/SingInRoutes.kt
@@ -4,12 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.make_your_skill.helpers.retrofit.auth.AuthRetrofit
+import com.make_your_skill.helpers.retrofit.auth.GetAuthService
 
 
 @Composable
 fun SingInRoutes(
     navController: NavHostController,
-    viewModel: SingInViewModel = viewModel()
+    viewModel: SingInViewModel = viewModel(factory = SingInViewModel.provideFactory(GetAuthService(AuthRetrofit()))) //Aca estas dejando un Default muy complejo, pero ya esta arreglado.
 ){
     SignInScreen(navController = navController)
 }
