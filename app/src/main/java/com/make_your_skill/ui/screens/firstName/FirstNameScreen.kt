a1b198907a2412d50ec4ee44f76440ae58a34202
AlexLangman <alulang@hotmail.com>
2024-11-04 22:59:25 -0300

    adding validations in name page

--- app/src/main/java/com/make_your_skill/ui/screens/firstName/FirstNameScreen.kt
+++ app/src/main/java/com/make_your_skill/ui/screens/firstName/FirstNameScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,32 +18,30 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.make_your_skill.ui.components.BackButton
 import com.make_your_skill.ui.components.CustomButton
 import com.make_your_skill.ui.components.CustomTextField
 import com.make_your_skill.ui.components.ScreenTitleText
-import com.make_your_skill.ui.navigation.AppRoutes
+import com.make_your_skill.ui.screens.createNewAccount.CreateNewAcoountViewModel
 
 @Composable
 fun FirstNameScreen(navController: NavHostController) {
-    var text by remember { mutableStateOf("") }
+    val createNewAcoountViewModel: CreateNewAcoountViewModel = hiltViewModel()
+    val viewModel: FirstNameViewModel = viewModel()
+    val firstname by createNewAcoountViewModel.firstname.collectAsState()
+    val lastname by createNewAcoountViewModel.lastname.collectAsState()
+    val error by viewModel.error.collectAsState()
+
     val separation = 25.dp
     val BUTTON_TEXT = "CONTINUE 1/4"
-    val FIRST_TEXT = "My first"
+    val FIRST_TEXT = "My full"
     val SECOND_TEXT = "name is..."
-    val LABEL = "Name..."
-
-    val onTextChange: (String) -> Unit = { newText ->
-        text = newText
-    }
-
-    val onClick = {
-        if (text != ""){
-            navController.navigate(AppRoutes.BIRTHDAY_SCREEN)
-        }
-    }
+    val FIRSTNAME_LABEL = "First Name..."
+    val LASTNAME_LABEL = "Last Name..."
 
     Column(
         modifier = Modifier
@@ -53,16 +54,26 @@ fun FirstNameScreen(navController: NavHostController) {
         Row {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxWidth() // Ocupa todo el ancho
+                    .fillMaxHeight(0.7f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
                 ScreenTitleText(FIRST_TEXT)
                 ScreenTitleText(SECOND_TEXT)
-                CustomTextField(text,onTextChange,LABEL, onSubmit = {}, false)
+                CustomTextField(firstname,createNewAcoountViewModel.onFirstNameChange,FIRSTNAME_LABEL, onSubmit = {}, false)
+                CustomTextField(lastname,createNewAcoountViewModel.onLastNameChange,LASTNAME_LABEL, onSubmit = {}, false)
+                if (error != null){
+                    Text(
+                        text = error.toString(),
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
         Row {
-            CustomButton(onClick,BUTTON_TEXT)
+            CustomButton({viewModel.onClick(firstname,lastname,navController)},BUTTON_TEXT)
         }
     }
 }
