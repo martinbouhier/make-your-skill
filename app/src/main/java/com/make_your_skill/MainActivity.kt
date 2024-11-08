512892477b43789532451367e0f1a8565ebd5424
LucaPoltiGit <luca.tobias.polti@gmail.com>
2024-11-08 20:22:52 -0300

    fixTamanosBottomBar

--- app/src/main/java/com/make_your_skill/MainActivity.kt
+++ app/src/main/java/com/make_your_skill/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.make_your_skill
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
