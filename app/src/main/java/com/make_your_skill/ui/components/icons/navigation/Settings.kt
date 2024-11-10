ff95f5284b0e50fed34348e1eca1f57a286e289e
LucaPoltiGit <luca.tobias.polti@gmail.com>
2024-11-10 01:01:38 -0300

    add topBar

---
+++ app/src/main/java/com/make_your_skill/ui/components/icons/navigation/Settings.kt
@@ -0,0 +1,9 @@
+package com.make_your_skill.ui.components.icons.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.make_your_skill.R
+
+@Composable
+fun getIconSettings(): Painter = painterResource(id = R.drawable.settings_icon)
