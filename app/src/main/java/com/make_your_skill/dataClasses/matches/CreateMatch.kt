b46ab3787af12ab5d864ecb42e7aeb32646bc53d
AlexLangman <alulang@hotmail.com>
2024-11-12 00:00:36 -0300

    adding match funcionality

--- app/src/main/java/com/make_your_skill/dataClasses/matches/CreateMatch.kt
+++ app/src/main/java/com/make_your_skill/dataClasses/matches/CreateMatch.kt
@@ -3,6 +3,6 @@ package com.make_your_skill.dataClasses.matches
 data class CreateMatchDto(
     val userAId: Int,
     val userBId: Int,
-    val skillAId: Int,
+    val skillAId: Int?,
     val skillBId: Int
 )
