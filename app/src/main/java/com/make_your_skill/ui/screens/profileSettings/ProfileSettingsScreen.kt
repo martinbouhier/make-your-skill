6f086373db47135ce5dc9846e4d1698cc53d607f
Marto96Bik <martobik@gmail.com>
2024-11-12 18:17:00 -0300

    ProfileSettingsScreen: Se remueve Preview

--- app/src/main/java/com/make_your_skill/ui/screens/profileSettings/ProfileSettingsScreen.kt
+++ app/src/main/java/com/make_your_skill/ui/screens/profileSettings/ProfileSettingsScreen.kt
@@ -133,9 +133,3 @@ fun EditProfile(
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun ProfileSettingsScreenPreview() {
-    ProfileSettingsScreen(rememberNavController(), SingInViewModel())
-}
